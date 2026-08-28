524ef6e16fa94f315ee6d81c642e1f5faaa08394
NaivoRzk <andrydev.tech@gmail.com>
2026-08-28 14:15:49 +0300

    [FEAT] ajouter la structure des dossiers et fichiers pour les composants et pages

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function Home(){
+    return(
+        <div>
+          <h1>Razakanaivo Andry Malala</h1>
+          <h2>Développeur Fullstack Python, JS & ERP Odoo</h2>
+        <p>
+          Passionné par l'ingénierie logicielle, je considère que les langages de programmation, les méthodologies de développement et les architectures ne sont que des outils permettant de résoudre des problèmes réels.<br />
+          J'utilise Python, JavaScript et Odoo pour concevoir et intégrer des solutions adaptées à chaque besoin.
+       </p>
+           <button>Télécharger CV</button>
+      </div>
+ );
+}
